38d2bf4d951eed3ce7c855b8185a0da05842f6ee
code-xue <17215444@cumt.edu.cn>
2022-03-02 23:59:14 +0800

    03_2

---
+++ 字符串函数2/test.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+
+
+//*-******************************************************
+//strstr函数：判断前一个字符串中是否包含后一个字符串
+
+//暴力比较法
+char* my_strstr(const char* str1, const char* str2)
+{
+	assert(str1 && str2);
+	const char* s1 = NULL;
+	const char* s2 = NULL;
+	const char* cp = str1;
+	while (*cp)
+	{
+		s1 = cp;
+		s2 = str2;
+		while (*s1 && *s2 && (*s1 == *s2))
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')
+		{
+			return cp;
+		}
+	}
+	return NULL;
+
+}
+
+
+int main()
+{
+	char arr1[] = "abcdefg";
+	char arr2[] = "cde";
+	char* ret = my_strstr(arr1, arr2);
+	if (ret != NULL)
+	{
+		printf("%s\n", arr1);
+	}
+	else
+	{
+		printf("找不到\n");
+	}
+	return 0;
+}
+
+//int main()
+//{
+//	int a = 8;
+//	printf("%d\n", a);
+//	return 0;
+//}
