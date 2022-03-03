b99f8c26881515f9637070d2dc9491ae8a418aa3
code-xue <17215444@cumt.edu.cn>
2022-03-03 08:34:05 +0800

    03_3

--- 字符串函数2/test.c
+++ 字符串函数2/test.c
@@ -49,3 +49,46 @@ int main()
 	}
 	return 0;
 }
+
+
+//*****************************************************************
+//strtok函数：将字符串按照特征字符分割
+//char * strtok ( char * str, const char * delimiters );
+//参数中前一个指针不为空，从第一个字符开始找，找到特征字符后将其改为\0，并在这个位置作上标记，返回开始处的地址
+//参数中前一个指针为NULL，从标记位置开始往后找，并在找到特征字符时返回开始处的地址
+//找不到特征字符时，返回NULL
+
+int main()
+{
+	char arr[] = "1282154067@qq.com";
+	char* p = "@.";//将@和.作为特征字符进行分割
+	//strtok函数会改变字符串内容，所以一般先将字符串复制一份
+	char tmp[20] = { 0 };
+	strcpy(tmp, arr);
+	char* ret = NULL;
+	for (ret = strtok(tmp, p); ret != NULL; ret = strtok(NULL, p))
+	{
+		printf("%s\n", ret);
+	}
+	
+    return 0;
+}
+
+//*****************************************************
+//strerror函数：读取库函数错误码的信息
+
+#include<errno.h>
+int errno;//保存错误码
+
+int main()
+{
+	FILE* pf = fopen("test.txt", "r");
+	if (pf == NULL)
+	{
+		printf("%s\n", strerror(errno));
+		return 1;
+	}
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
