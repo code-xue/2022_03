525d708e706351157d723e12a79825a25c6ba366
code-xue <17215444@cumt.edu.cn>
2022-03-13 20:49:11 +0800

    03_13

--- 作业/test.c
+++ 作业/test.c
@@ -1,5 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<ctype.h>
 
 //***************************************************************************
 //1.多组输入，输出n行的X形图案
@@ -108,5 +111,172 @@ int main()
 	{
 		printf("%d ", arr[i]);
 	}
+	return 0;
+}
+
+
+//***************************************************************************************
+//模拟实现atoi函数：将字符串转化成数字
+
+enum STATE
+{
+	INVALID,
+	VALID
+};
+
+enum STATE state = INVALID;
+
+int my_atoi(const char* p)
+{
+	//p为空指针，非法转化
+	if (p == NULL)
+	{
+		return 0;
+	}
+	//空字符串，非法转化
+	if (*p == '\0')
+	{
+		return 0;
+	}
+	//前面有空格，跳过空格
+	while (isspace(*p))
+	{
+		p++;
+	}
+	//判断数字正负
+	int flag = 1;
+	if (*p == '-')
+	{
+		flag = -1;
+		p++;
+	}
+	//数字的转化
+	long long n = 0; //防止数字过大
+	while (isdigit(*p))
+	{
+		n = n * 10 + flag * (*p - '0');
+		//判断数字是否超出整型的表示范围
+		if (n > INT_MAX || n < INT_MIN)
+		{
+			return 0;
+		}
+		p++;
+	}
+	state = VALID;
+	return (int)n;
+	
+}
+
+int main()
+{
+	const char* p = "    -125748a";
+	int ret = my_atoi(p);
+	//判断是合法转化还是非法转化
+	if (state == INVALID)
+	{
+		printf("非法的转化：");
+	}
+	else
+	{
+		printf("合法的转化：");
+	}
+
+	printf("%d\n", ret);
+
+	return 0;
+}
+
+
+//****************************************************************
+//有一组数字中有两个数字值出现一次，其他数字都出现两次，找出这两个数并打印
+//思路：将所有数字异或， 相同两个数字异或为0，最后结果就是要找的两个数异或的结果
+//找出结果中哪一位二进制为1，按这一位进行将所有数字进行分组，两组分别异或
+
+void Find(int arr[], int sz, int* px, int* py)
+{
+	//1.先将所有数字异或
+	int i = 0;
+	int ret = 0;
+	for (i = 0; i < sz; i++)
+	{
+		ret ^= arr[i];
+	}
+	//2.找出ret中哪一位为1
+	int pos = 0;
+	for (i = 0; i < 32; i++)
+	{
+		if (((ret >> i) & 1) == 1)
+		{
+			pos = i;
+		}
+	}
+	//3.分两组分别异或
+	int num1 = 0;
+	int num2 = 0;
+	for (i = 0; i < sz; i++)
+	{
+		if (((arr[i] >> pos) & 1) == 1)
+		{
+			num1 ^= arr[i];
+		}
+		else
+		{
+			num2 ^= arr[i];
+		}
+	}
+	*px = num1;
+	*py = num2;
+}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,5,4,3,2,1 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int x = 0;
+	int y = 0;
+	Find(arr, sz, &x, &y);
+	printf("%d %d\n", x, y);
+	return 0;
+}
+
+
+//*********************************************************************************
+//写一个宏，可以将一个整数的二进制位的奇数位和偶数位互换
+//思路：将这个数按位与10101010101010101010101010101010，得到二进制的偶数位
+//      将这个数按位与01010101010101010101010101010101，得到二进制的奇数位
+//再将偶数位右移一位，奇数位左移一位，再相加就得到最后结果
+
+#define SWAP(num) ((num&0xaaaaaaaa)>>1)+((num&0x55555555)<<1)
+
+int main()
+{
+	int a = 10;
+	printf("%d\n", SWAP(a));
+	return 0;
+}
+
+
+//********************************************************************************
+//写一个宏，计算结构体中某变量相对于首地址的偏移
+
+#include<stddef.h>
+
+struct S
+{
+	int a;
+	short b;
+	int c;
+	char d;
+};
+
+//将0强转为结构体指针，成员的地址就是偏移量
+#define OFFSETOF(struct_name,mem_name)  (int)&(((struct_name*)0)->mem_name)
+
+int main()
+{
+	printf("%d\n", OFFSETOF(struct S, a));
+	printf("%d\n", OFFSETOF(struct S, b));
+	printf("%d\n", OFFSETOF(struct S, c));
+	printf("%d\n", OFFSETOF(struct S, d));
 	return 0;
 }
